91a6530cdcb502ec4fda605c2dba44bbef95aa24
ivan <ivan.shi@dunqian.tw>
2023-05-17 10:04:58 +0800

    feat: 優化

--- config/oauth.ts
+++ config/oauth.ts
@@ -1,13 +1,17 @@
-export default {
-    // OAuth 2.0
-    clientId: "2",
-    clientSecret: "O109bLD^thr6m$r^dhnwZaAr^okbadG5QO5Thg5&",
-    userAuthorizationUri: "https://accounts.itrd.tw/oauth/authorize",
-    accessTokenUri: "https://accounts.itrd.tw/oauth/token",
-    userInfoUri: "https://accounts.itrd.tw/oauth/me",
-    redirectUri: "http://localhost:3000/oauth/callback",
-    scope: "members",
-    state: "",
-    responseType: "code",
-    grantType: "authorization_code",
+export const useOauthConfig = () => {
+    const runtimeConfig = useRuntimeConfig();
+    return {
+        // OAuth 2.0
+        clientId: runtimeConfig.public.clientId!, // 會員中心client_id
+        clientSecret: runtimeConfig.public.clientSecret!, // 會員中心client_secret
+        userAuthorizationUri: runtimeConfig.public.userAuthorizationUri!, // 會員中心授權登入網址
+        accessTokenUri: runtimeConfig.public.accessTokenUri!, // 會員中心取得token網址
+        userInfoUri: runtimeConfig.public.userInfoUri!, // 會員中心取得使用者資料網址
+        exchangeTokenUri: runtimeConfig.public.exchangeTokenUri!, // 交換權杖網址
+        redirectUri: runtimeConfig.public.redirectUri!, // 會員中心授權登入後的轉址網址（需改成自己的網址）
+        scope: "", // 會員中心授權範圍
+        state: "",
+        responseType: "code",
+        grantType: "authorization_code",
+    }
 };
